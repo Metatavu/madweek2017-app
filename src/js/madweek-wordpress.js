ada27aaadda7c3fdef912202adf1d3f7a9ad6cf1
Antti Leppä <antti.leppa@metatavu.fi>
2017-08-09 13:46:44 +0300

    initial

---
+++ src/js/madweek-wordpress.js
@@ -0,0 +1,41 @@
+/* jshint esversion: 6 */
+/* global getConfig, StatusBar, WPAPI, Promise */
+
+(function(){
+  'use strict';
+  
+  $.widget("custom.madweekWordpress", {
+    
+    options: {
+      host: 'www.madmikkeli.com',
+      port: 80,
+      secure: false
+    },
+    
+    _create : function() {
+      this.url = `${this.options.secure ? 'https' : 'http'}://${this.options.host}:${this.options.port}/wp-json`;
+      this.api = new WPAPI({ 
+        endpoint: this.url
+      });
+      
+      this.api.events = this.api.registerRoute( 'eventon', '/events/(?P<id>)');
+    },
+    
+    listEvents: function () {
+      return this.api.events()
+        .then((result) => {
+          const events = [];
+          
+          _.forEach(result.events, (event, id) => {
+            events.push(Object.assign(event, {
+              id: id
+            }));
+          });
+          
+          return events;
+        });
+    }
+    
+  });
+
+})();
--- src/js/madweek.js
+++ src/js/madweek.js
@@ -10,6 +10,22 @@
     },
     
     _create : function() {
+      $(this.element).madweekWordpress();
+      $(this.element).madweekWordpress('listEvents')
+        .then((events) => {
+          for (let i = 0; i < events.length; i++) {
+            const event = events[i];
+    
+            const html = pugEvent({
+              event: event
+            });
+
+            $('.events-container').append(html);
+          }
+        })
+        .catch((err) => {
+          console.log("ERR:" + err);
+        });
     }
     
   });
